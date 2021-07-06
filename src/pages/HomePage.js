7a3870214b887037dac6b3ef31e27c23c853e0db
jordi <romero.suarez.jordi@gmail.com>
2021-07-06 12:54:16 +0200

    Modificado los tabs de la home page y pasados parametros de las cards

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,45 +1,37 @@
 import React from 'react'
 import {Grid, Typography} from "@material-ui/core";
 import AppFrame from "../components/AppFrame";
-import NavBar from "../components/NavBar";
-import Form from "../components/Form";
-import DataTable from "../components/DataTable";
 import StudiesCard from "../components/StudiesCard";
 
+
 const HomePage = () => {
+    const tabs = [
+        {
+            labelTab: "Home",
+            linkTab: "/",
+        }
+    ];
     return (
-        <AppFrame>
+        <AppFrame tabs={tabs}>
             <Grid item style={{marginTop: "5em"}}>
                 <Grid container item direction={"column"} style={{marginBottom: "2em", padding: "10px"}}>
-                    <Typography variant={"h3"} align={"center"}>Welcome to XXXXX</Typography>
+                    <Typography variant={"h3"} align={"center"}>Welcome to OpenMRS</Typography>
                     <Typography variant={"subtitle1"} align={"center"}>Lorem Ipsum</Typography>
                 </Grid>
             </Grid>
 
 
-            <Grid container
-                  direction={"column"}
-                  justify="center">
-                <Grid item>
-                    <Form/>
-                </Grid>
-            </Grid>
-
-            <Grid container
-                  direction={"column"}
-                  justify="center"
-                  style={{margin: "2em", padding: "10px"}}>
-                <Grid item>
-                    <DataTable/>
-                </Grid>
+            <Grid container xs={12} spacing={1}>
+                <StudiesCard path={`study/Study 1/forms`}
+                             name={"Study 1"}
+                             description={"LOREM 1"}/>
+                <StudiesCard path={`study/Study 2/forms`}
+                             name={"Study 2"}
+                             description={"LOREM 2"}/>
             </Grid>
 
 
-            <Grid container xs={12} spacing={1} >
-                    <StudiesCard/>
-                    <StudiesCard/>
 
-            </Grid>
 
         </AppFrame>
     )
