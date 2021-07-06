e2646c6d739b22cf4bd7dd82c06cae91dd145469
jordi <romero.suarez.jordi@gmail.com>
2021-07-06 12:52:14 +0200

    Actualizado para poner los tabs segun el view

--- src/components/AppFrame/AppFrame.js
+++ src/components/AppFrame/AppFrame.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid'
 import NavBar from "../NavBar";
 import 'typeface-roboto'
+import {COLOR_BACKGROUND, COLOR_BLUE} from "../../constants/colors";
 
-const AppFrame = ({children}) => {
+const AppFrame = ({children, tabs}) => {
     return (
         <Grid container
+              style={{backgroundColor: COLOR_BACKGROUND}}
               direction={"column"}
               justify="center">
             <Grid item>
-                <NavBar/>
+                <NavBar tabs={tabs}/>
             </Grid>
             {children}
         </Grid>
