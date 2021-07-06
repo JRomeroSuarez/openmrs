e182afeef9e1ad47668368cb1c9d30afa3e53d8f
jordi <romero.suarez.jordi@gmail.com>
2021-07-06 12:51:19 +0200

    Añadidos nuevos paths para las views nuevas Queda revisar porque hay una que no lleva a la nada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Button from '@material-ui/core/Button';
 import HomePage from './pages/HomePage'
+import DataPage from "./pages/DataPage";
+import FormsPage from "./pages/FormsPage";
+import ParticipantsPage from "./pages/ParticipantsPage";
+
 function App() {
   return (
       <Router>
           <Switch>
               <Route exact path="/">
                   <HomePage/>
+              </Route>
+              <Route exact path="/study/:studyName/forms/:formName">
+                  <DataPage/>
+              </Route>
+              <Route exact path="/study/:studyName/forms">
+                  <FormsPage/>
+              </Route>
+              <Route exact path="/study/:studyName/participants">
+                  <ParticipantsPage/>
               </Route>
           </Switch>
       </Router>
