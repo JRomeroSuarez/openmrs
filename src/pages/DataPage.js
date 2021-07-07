5f273aca318e4cc4e2915a23ff3b21af94d398d1
Jordi <romero.suarez.jordi@gmail.com>
2021-07-07 11:22:36 +0200

    Moved the data to the directory and changed endpoints

--- src/pages/DataPage.js
+++ src/pages/DataPage.js
@@ -2,12 +2,18 @@ import React from 'react'
 import {Grid, Typography} from "@material-ui/core";
 import AppFrame from "../components/AppFrame";
 import DataTable from "../components/DataTable";
+import {useLocation} from "react-router-dom";
 
+import {headTable, reports} from "../data/reports";
 
 const DataPage = () => {
+
+    const location = useLocation();
+    let path_array = location.pathname.split("/");
     const tabs = [
         {
-            labelTab: "Study 1",
+            labelTab: path_array[path_array.length - 3],
+            linkTab: "/study/" + path_array[path_array.length - 3] + "/forms",
         },
         {
             labelTab: "Home",
@@ -15,140 +21,22 @@ const DataPage = () => {
         },
         {
             labelTab: "Forms",
-            linkTab: "/study/Study 1/forms",
+            linkTab: "/study/" + path_array[path_array.length - 3] + "/forms",
         },
         {
             labelTab: "Participants",
-            linkTab: "/study/Study 1/participants"
+            linkTab: "/study/" + path_array[path_array.length - 3] + "/participants",
         }
     ];
 
-    const columns = [
-        {field: 'col1', headerName: 'Question', width: 200},
-        {field: 'col2', headerName: 'Subtitle', width: 150},
-        {field: 'col3', headerName: 'Info', width: 150},
-        {field: 'col4', headerName: 'Field type', width: 150},
-        {field: 'col5', headerName: 'Mandatory', width: 150},
-        {field: 'col6', headerName: 'Variable name', width: 150},
-        {field: 'col7', headerName: 'Conditional', width: 150},
-        {field: 'col8', headerName: 'Creation date', width: 150},
-        {field: 'col9', headerName: 'Edit date', width: 150},
-        {field: 'col10', headerName: 'Actions', width: 150},
-    ];
-
-    const rows = [
-        {
-            id: 1,
-            col1: 'Lorem ',
-            col2: '0',
-            col3: '',
-            col4: 'Range',
-            col5: 'Y',
-            col6: 'POS',
-            col7: 'Y',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 2,
-            col1: 'Lorem',
-            col2: '1',
-            col3: '',
-            col4: 'Info',
-            col5: 'N',
-            col6: 'POS',
-            col7: 'N',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 3,
-            col1: 'Lorem',
-            col2: '2',
-            col3: '',
-            col4: 'Text',
-            col5: 'N',
-            col6: 'POS',
-            col7: 'N',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 4,
-            col1: 'Lorem',
-            col2: '3',
-            col3: '',
-            col4: 'Single',
-            col5: 'Y',
-            col6: 'POS',
-            col7: 'N',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 5,
-            col1: 'Lorem',
-            col2: '4',
-            col3: '',
-            col4: 'Info',
-            col5: 'Y',
-            col6: 'POS',
-            col7: 'Y',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 6,
-            col1: 'Lorem',
-            col2: '5',
-            col3: '',
-            col4: 'Text',
-            col5: 'Y',
-            col6: 'POS',
-            col7: 'N',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 7,
-            col1: 'Lorem',
-            col2: '6',
-            col3: '',
-            col4: 'Range',
-            col5: 'Y',
-            col6: 'POS',
-            col7: 'N',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-        {
-            id: 8,
-            col1: 'Lorem',
-            col2: '7',
-            col3: '',
-            col4: 'Single',
-            col5: 'N',
-            col6: 'POS',
-            col7: 'Y',
-            col8: '1-1-1111',
-            col9: '1-1-1111',
-            col10: 'Y'
-        },
-    ];
     return (
 
         <AppFrame tabs={tabs}>
 
             <Grid item style={{marginTop: "5em"}}>
                 <Grid container item direction={"column"} style={{marginBottom: "2em", padding: "10px"}}>
-                    <Typography variant={"h3"} align={"center"}>Forms del Study 1</Typography>
+                    <Typography variant={"h3"} align={"center"}>Form
+                        de {path_array[path_array.length - 1]}</Typography>
                     <Typography variant={"subtitle1"} align={"center"}>Lorem Ipsum</Typography>
                 </Grid>
             </Grid>
@@ -157,7 +45,7 @@ const DataPage = () => {
                   justify="center"
                   style={{margin: "2em", padding: "10px"}}>
                 <Grid item>
-                    <DataTable rows={rows} columns={columns}/>
+                    <DataTable rows={reports} columns={headTable}/>
                 </Grid>
             </Grid>
         </AppFrame>
--- src/pages/ParticipantsPage.js
+++ src/pages/ParticipantsPage.js
@@ -4,13 +4,15 @@ import AppFrame from "../components/AppFrame";
 import DataTable from "../components/DataTable";
 import {useLocation} from "react-router-dom";
 
+import {headTable, participants} from "../data/participants";
 
 const ParticipantsPage = () => {
     const location = useLocation();
     let path_array = location.pathname.split("/");
     const tabs = [
         {
-            labelTab: "Study 1",
+            labelTab: path_array[path_array.length - 2],
+            linkTab: "/study/" + path_array[path_array.length - 2] + "/forms",
         },
         {
             labelTab: "Home",
@@ -18,39 +20,19 @@ const ParticipantsPage = () => {
         },
         {
             labelTab: "Forms",
-            linkTab: "/study/Study 1/forms",
+            linkTab: "/study/" + path_array[path_array.length - 2] + "/forms",
         },
         {
             labelTab: "Participants",
-            linkTab: "/study/Study 1/participants"
+            linkTab: "/study/" + path_array[path_array.length - 2] + "/participants",
         }
-    ];
-
-    const columns = [
-        {field: 'col1', headerName: 'Mail', width: 200},
-        {field: 'col2', headerName: 'ID', width: 150},
-        {field: 'col3', headerName: 'Associated forms', width: 150},
-        {field: 'col4', headerName: 'Forms completed', width: 150},
-        {field: 'col5', headerName: 'Languaje', width: 150},
-        {field: 'col6', headerName: 'Invitation status', width: 150},
-        {field: 'col7', headerName: 'Actions', width: 150},
-    ];
-
-    const rows = [
-        {id: 1, col1: 'Example@iGive2.com', col2: 'St11111', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 2, col1: 'Example2@iGive2.com', col2: 'St22222', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 3, col1: 'Example3@iGive2.com', col2: 'St33333', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 4, col1: 'Example4@iGive2.com', col2: 'St44444', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 5, col1: 'Example5@iGive2.com', col2: 'St55555', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 6, col1: 'Example6@iGive2.com', col2: 'St66666', col3: '', col4: '', col5: '', col6: 'Accepted', col7: ''},
-        {id: 7, col1: 'Example7@iGive2.com', col2: 'St77777', col3: '', col4: '', col5: '', col6: 'Pending', col7: ''},
-        {id: 8, col1: 'Example8@iGive2.com', col2: 'St88888', col3: '', col4: '', col5: '', col6: 'Rejected', col7: ''},
     ];
     return (
         <AppFrame tabs={tabs}>
             <Grid item style={{marginTop: "5em"}}>
                 <Grid container item direction={"column"} style={{marginBottom: "2em", padding: "10px"}}>
-                    <Typography variant={"h3"} align={"center"}>Participants de {path_array[path_array.length - 2]}</Typography>
+                    <Typography variant={"h3"} align={"center"}>Participants
+                        de {path_array[path_array.length - 2]}</Typography>
                     <Typography variant={"subtitle1"} align={"center"}>Lorem Ipsum</Typography>
                 </Grid>
             </Grid>
@@ -59,7 +41,7 @@ const ParticipantsPage = () => {
                   justify="center"
                   style={{margin: "2em", padding: "10px"}}>
                 <Grid item>
-                    <DataTable rows={rows} columns={columns}/>
+                    <DataTable rows={participants} columns={headTable}/>
                 </Grid>
             </Grid>
 
