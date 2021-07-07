ebdfd1017de88e7cd9ef75fd19dc97a617c323f5
Jordi <romero.suarez.jordi@gmail.com>
2021-07-07 14:03:50 +0200

    Function to donwload forms of each study

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {DataGrid} from '@material-ui/data-grid';
 import {makeStyles} from '@material-ui/core/styles';
+import AppIconButton from "../AppIconButton";
+import {nameIcons} from "../../constants/icons";
+import {useLocation} from "react-router-dom";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +16,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+
+
 const DataTable = ({rows, columns}) => {
+
+    const downloadFile =() =>{
+        const name = path_array[path_array.length - 1]
+        const link = document.createElement('a');
+        const nameCleaned = name.replace(/\s/g, '_')
+        link.download = `${nameCleaned}.csv`;
+        link.href = "./dataset.csv";
+        link.click();
+    }
+
+    const location = useLocation();
+    let path_array = location.pathname.split("/");
+
     const classes = useStyles();
 
     const [pageSize, setPageSize] = React.useState(10);
@@ -26,6 +44,7 @@ const DataTable = ({rows, columns}) => {
             <DataGrid pageSize={pageSize}
                       onPageSizeChange={handlePageSizeChange}
                       rowsPerPageOptions={[10, 20, 30]} pagination rows={rows} columns={columns}/>
+            <AppIconButton name={nameIcons.DOWNLOAD} text={"Download Data"} onClick={() => downloadFile()}/>
         </div>
     )
 }
