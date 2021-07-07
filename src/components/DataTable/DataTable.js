10c8147e27b0556ee3d10b9002b96b4baf713585
Jordi <romero.suarez.jordi@gmail.com>
2021-07-07 11:23:56 +0200

    Delete default database and modified the size of the table

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import {DataGrid, GridColDef, GridRowsProp} from '@material-ui/data-grid';
+import {DataGrid} from '@material-ui/data-grid';
 import {makeStyles} from '@material-ui/core/styles';
 
-const columns: GridColDef[] = [
-    {field: 'col1', headerName: 'Column 1', width: 150},
-    {field: 'col2', headerName: 'Column 2', width: 150},
-    {field: 'col3', headerName: 'Column 3', width: 150},
-    {field: 'col4', headerName: 'Column 4', width: 150},
-];
-
-const rows: GridRowsProp = [
-    {id: 1, col1: 'a', col2: 'b', col3: 'c', col4: 'd'},
-    {id: 2, col1: 'e', col2: 'f', col3: 'g', col4: 'h'},
-    {id: 3, col1: 'i', col2: 'World', col3: 'World', col4: 'World'},
-    {id: 4, col1: 'Hello', col2: 'World', col3: 'World', col4: 'World'},
-    {id: 5, col1: 'Hello', col2: 'World', col3: 'World', col4: 'World'},
-    {id: 6, col1: 'Hello', col2: 'World', col3: 'World', col4: 'World'},
-];
 
 const useStyles = makeStyles((theme) => ({
     table: {
         margin: theme.spacing(2),
-        height: 500, width: '80%'
+        height: 500, width: '80%',
+
     },
 
 }));
+
+
 const DataTable = ({rows, columns}) => {
     const classes = useStyles();
+
+    const [pageSize, setPageSize] = React.useState(5);
+    const handlePageSizeChange = (params) => {
+        setPageSize(params.pageSize);
+    };
+
     return (
         <div className={classes.table}>
-            <DataGrid rows={rows} columns={columns}/>
+            <DataGrid pageSize={pageSize}
+                      onPageSizeChange={handlePageSizeChange}
+                      rowsPerPageOptions={[5, 10, 20]} pagination rows={rows} columns={columns}/>
         </div>
     )
 }
