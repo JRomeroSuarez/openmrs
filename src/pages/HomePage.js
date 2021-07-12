7790bd83a5833cdf8097e5301d306ee02c414868
jordi <romero.suarez.jordi@gmail.com>
2021-07-12 16:04:23 +0200

    Clean code, adapt the code to the JSON file and started the axios auth

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {Grid, Typography} from "@material-ui/core";
 import AppFrame from "../components/AppFrame";
 import StudiesCard from "../components/StudiesCard";
 import {listStudies} from "../data/studies";
-
+import Studies from "../endpointsPostman/studies.json"
+import axios from "axios";
+import Button from "@material-ui/core/Button";
 
 const HomePage = () => {
+/*
+    const accesToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYyODY3MTgxM30.rj7dC3pKg5GNsFiZWVLnojZ5l4sf1uIJRZWSfwAa6LP2repL4YqUjXvdtOCswyCo7mFz6qvOZozWCo9-HkTcgA'
+    const apiUrl = 'http://localhost:8080/api'
+
+    const [studiesData, setStudiesData] = useState([]);
+    const studiesUrl = "http://localhost:8080/api/studies";
+
+    const authAxios = axios.create({
+        baseURL: apiUrl,
+        headers: {
+            Authorization: `Bearer ${accesToken}`,
+            Acces:``
+        }
+    })
+    const getStudiesWithAxios = useCallback(async () => {
+        const response = await authAxios.get(`/studies`);
+        setStudiesData(response.data);
+
+    });
+
+    useEffect(() => {
+        getStudiesWithAxios();
+
+    }, []);*/
+
     const tabs = [
         {
             labelTab: "Home",
@@ -14,6 +41,7 @@ const HomePage = () => {
     ];
     return (
         <AppFrame tabs={tabs}>
+
             <Grid item style={{marginTop: "5em"}}>
                 <Grid container item direction={"column"} style={{marginBottom: "2em", padding: "10px"}}>
                     <Typography variant={"h3"} align={"center"}>Welcome to OpenMRS</Typography>
@@ -24,12 +52,12 @@ const HomePage = () => {
 
             <Grid container xs={12} spacing={1}>
 
-                {listStudies.map(item => (
-                    <StudiesCard name={item.name}
-                                 path={`study/${item.name}/forms`}
+                {Studies.map(item => (
+                    <StudiesCard name={item.title}
+                                 path={`study/${item.title}/forms`}
                                  description={item.description}
-                                 participants={item.participants}
-                                 forms={item.forms}
+                                 participants={item.numParticipants}
+                                 forms={item.numForms}
                     />
                 ))}
 
